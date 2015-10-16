63a70920fb9869ecaa2e71b0b5a656c8545c22f3
SukeshBolar123 <sukesh@codecraft.co.in>
2015-10-16 15:45:37 +0530

    Added new project - Jenkins demo

---
+++ src/test/java/TestProject/Demo/TestSequence.java
@@ -0,0 +1,10 @@
+package TestProject.Demo;
+
+import org.testng.annotations.Test;
+
+public class TestSequence {
+  @Test
+  public void f() {
+	  System.out.println("Hey this is only testing.......");
+  }
+}
